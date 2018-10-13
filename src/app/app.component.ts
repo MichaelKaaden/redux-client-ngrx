bbab69bbad4d45c1bdd11ccad00d9f8e1ccd1531
Michael Kaaden <github@kaaden.net>
2018-10-13 09:13:35 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mk-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'redux-client-ngrx';
+}
