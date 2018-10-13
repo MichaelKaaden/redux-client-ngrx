f1257c488f26699846932a3fbe15dd1d97a6010f
Michael Kaaden <github@kaaden.net>
2018-10-13 21:45:40 +0200

    Add a material module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,14 @@
-import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "./material.module";
 
 @NgModule({
     declarations: [AppComponent],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
     providers: [],
     bootstrap: [AppComponent],
 })
