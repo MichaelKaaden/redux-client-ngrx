8c1940ffb973ff1d1316b410913e49cfaf90bbc0
Michael Kaaden <github@kaaden.net>
2018-11-02 22:09:34 +0100

    [chore] Move app.component into the components directory

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { AppComponent } from "./components/app/app.component";
 import { MaterialModule } from "./material.module";
 
 @NgModule({
