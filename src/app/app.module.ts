3366bfc98ea3d18c5bc94e2a3c3ca01e916a591c
Michael Kaaden <github@kaaden.net>
2018-11-03 09:15:15 +0100

    Import the FlexLayoutModule (silly me!)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from "@angular/core";
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./components/app/app.component";
-import { MaterialModule } from "./material.module";
+import { CounterContainerComponent } from "./components/counter-container/counter-container.component";
 import { CounterListComponent } from "./components/counter-list/counter-list.component";
 import { DashboardComponent } from "./components/dashboard/dashboard.component";
-import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 import { ErrorComponent } from "./components/error/error.component";
-import { CounterContainerComponent } from "./components/counter-container/counter-container.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { MaterialModule } from "./material.module";
 
 @NgModule({
     declarations: [
@@ -20,7 +21,7 @@ import { CounterContainerComponent } from "./components/counter-container/counte
         ErrorComponent,
         CounterContainerComponent,
     ],
-    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule],
+    imports: [BrowserModule, AppRoutingModule, BrowserAnimationsModule, MaterialModule, FlexLayoutModule],
     providers: [],
     bootstrap: [AppComponent],
 })
