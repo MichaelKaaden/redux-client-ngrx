d93bbd1f50a8c9bfe554c5814885168299169075
Michael Kaaden <github@kaaden.net>
2018-11-04 11:14:54 +0100

    Fix tests for the counter effects

--- src/app/effects/counter.effects.spec.ts
+++ src/app/effects/counter.effects.spec.ts
@@ -1,16 +1,36 @@
+import { HttpClient } from "@angular/common/http";
 import { TestBed } from "@angular/core/testing";
 import { provideMockActions } from "@ngrx/effects/testing";
+import { combineReducers, StoreModule } from "@ngrx/store";
 import { Observable } from "rxjs";
+import * as fromCounter from "../reducers/counter.reducer";
+import { CounterService } from "../services/counter.service";
 
 import { CounterEffects } from "./counter.effects";
 
 describe("CounterEffects", () => {
-    const actions$: Observable<any>;
+    // tslint:disable-next-line:prefer-const
+    let actions$: Observable<any>;
     let effects: CounterEffects;
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            providers: [CounterEffects, provideMockActions(() => actions$)],
+            imports: [
+                StoreModule.forRoot({
+                    counters: combineReducers(fromCounter.reducer),
+                }),
+            ],
+            providers: [
+                CounterEffects,
+                provideMockActions(() => actions$),
+                CounterService,
+                // dependencies of CounterService, needed for instantiation
+                // never used because of the spies, therefore may be an empty object
+                {
+                    provide: HttpClient,
+                    useValue: {},
+                },
+            ],
         });
 
         effects = TestBed.get(CounterEffects);
