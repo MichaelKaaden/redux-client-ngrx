17609d11b195f8fc3a3e9af36f7e37c82418ade5
Michael Kaaden <github@kaaden.net>
2018-12-03 08:13:31 +0100

    Rename the State interfaces to either CountersState or ErrorsState

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -6,8 +6,8 @@ import * as fromCounter from "./counter.reducer";
 import * as fromError from "./error.reducer";
 
 export interface IAppState {
-    counters: fromCounter.State;
-    errors: fromError.State;
+    counters: fromCounter.CountersState;
+    errors: fromError.ErrorsState;
 }
 
 export const reducers: ActionReducerMap<IAppState> = {
@@ -25,23 +25,34 @@ export const metaReducers: MetaReducer<IAppState>[] = !environment.production ? 
 /*
  * Counter selectors
  */
-export const selectCountersState = createFeatureSelector<fromCounter.State>("counters");
-export const getCounters = createSelector(selectCountersState, fromCounter.getCounters);
-export const getCounter = createSelector(getCounters, (counters, props) =>
-    counters.find((counter) => counter.index === props.index)
+export const selectCountersState = createFeatureSelector<fromCounter.CountersState>("counters");
+export const getCounters = createSelector(
+    selectCountersState,
+    fromCounter.getCounters,
 );
-export const getNumOfCounters = createSelector(getCounters, (counters) => (!!counters ? counters.length : 0));
-export const getCounterSum = createSelector(getCounters, (counters) =>
-    counters.reduce((accumulator: number, current: ICounter) => accumulator + (current.value ? current.value : 0), 0)
+export const getCounter = createSelector(
+    getCounters,
+    (counters, props) => counters.find((counter) => counter.index === props.index),
+);
+export const getNumOfCounters = createSelector(
+    getCounters,
+    (counters) => (!!counters ? counters.length : 0),
+);
+export const getCounterSum = createSelector(
+    getCounters,
+    (counters) => counters.reduce((accumulator: number, current: ICounter) => accumulator + (current.value ? current.value : 0), 0),
 );
 export const getAverageSum = createSelector(
     getNumOfCounters,
     getCounterSum,
-    (num, sum) => (num && num !== 0 ? Number.parseFloat((sum / num).toFixed(2)) : 0)
+    (num, sum) => (num && num !== 0 ? Number.parseFloat((sum / num).toFixed(2)) : 0),
 );
 
 /*
  * Error selectors
  */
-export const selectErrorsState = createFeatureSelector<fromError.State>("errors");
-export const getErrors = createSelector(selectErrorsState, fromError.getErrors);
+export const selectErrorsState = createFeatureSelector<fromError.ErrorsState>("errors");
+export const getErrors = createSelector(
+    selectErrorsState,
+    fromError.getErrors,
+);
