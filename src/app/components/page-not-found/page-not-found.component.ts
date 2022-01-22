1499b873d2ddb7b6b294d0b5177f3dd00730a68e
Michael Kaaden <github@kaaden.net>
2022-01-22 10:49:31 +0100

    [eslint] Fix warning "Lifecycle methods should not be empty"

--- src/app/components/page-not-found/page-not-found.component.ts
+++ src/app/components/page-not-found/page-not-found.component.ts
@@ -1,12 +1,8 @@
-import { Component, OnInit } from "@angular/core";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "mk-page-not-found",
     templateUrl: "./page-not-found.component.html",
     styleUrls: ["./page-not-found.component.css"],
 })
-export class PageNotFoundComponent implements OnInit {
-    constructor() {}
-
-    ngOnInit() {}
-}
+export class PageNotFoundComponent {}
